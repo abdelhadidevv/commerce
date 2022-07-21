959eb0a1a2abdedead23f06a07296371b3f1c461
abdelhadidevv <88690811+abdelhadidevv@users.noreply.github.com>
2022-07-22 02:21:54 +0300

    edit service request

--- store/features/products/productsService.js
+++ store/features/products/productsService.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import stringify from "../../../utils/stringify";
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const offers = async () => {
--- store/features/user/userService.js
+++ store/features/user/userService.js
@@ -1,31 +1,22 @@
 import axios from "axios";
-import { authHeader } from "../../../utils/authHeader";
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const profile = async () => {
-  authHeader();
-
   const response = await axios.get(API_URL + "users/profile");
   return response.data;
 };
 
 const userOrders = async () => {
-  authHeader();
-
   const response = await axios.get(API_URL + "orders/my-orders");
   return response.data;
 };
 
 const addToCart = async (productData) => {
-  authHeader();
-
   const response = await axios.put(API_URL + "users/profile/cart", productData);
   return response.data;
 };
 
 const deleteFromCart = async (productId) => {
-  authHeader();
-
   const response = await axios.delete(
     API_URL + `users/profile/cart?productId=${productId}`
   );
